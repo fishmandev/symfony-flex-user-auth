a35e4bfe2f463ad37c1b9efb099d484417711c61
FishmanDev <fishman.code@gmail.com>
2017-08-07 11:32:03 +0300

    composer req twig

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
     'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
 ];
