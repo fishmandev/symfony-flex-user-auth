0b6a32783cbfbbc1165631808215dfa0992f36f6
FishmanDev <fishman.code@gmail.com>
2017-08-07 12:35:20 +0300

    Create 'security' controller, actions and template

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class SecurityController
+ * @package App\Controller
+ */
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $authUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction(AuthenticationUtils $authUtils)
+    {
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+    /**
+     * @Route("/admin/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+        // Does not need to be implemented
+    }
+}
